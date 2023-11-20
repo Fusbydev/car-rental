a7be386298582d90a4cd1eef118ac348befef80f
Fusbydev <kirkfabonnnn@gmail.com>
2023-11-20 11:39:57 +0800

    fixed the image layout

--- src/car/rental/system/carDescription.java
+++ src/car/rental/system/carDescription.java
@@ -7,8 +7,8 @@ package car.rental.system;
 import com.mysql.cj.jdbc.Blob;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
 import javax.swing.ImageIcon;
@@ -25,17 +25,26 @@ public class carDescription extends javax.swing.JPanel {
      * @param price
      * @param idcar 
      * @param availability*/
-    public carDescription(int price, int idcar, int availability, Blob blob) throws SQLException, FileNotFoundException, IOException {
+    public carDescription(float price, int idcar, int availability, Blob blob) throws SQLException, FileNotFoundException, IOException {
         initComponents();
         this.carID = idcar;
         this.availability = availability;
         setMaximumSize(new Dimension(127, 165));
         setVisible(true);
-        jLabel2.setText("PHP" + price);
-        try {
-        byte[] imageBytes = blob.getBytes(1, (int) blob.length());
-        ImageIcon icon = new ImageIcon(imageBytes);
-        imagePanel.setIcon(icon);
+        jLabel2.setText("PHP: " + price + " / Day");
+         try {
+            byte[] imageBytes = blob.getBytes(1, (int) blob.length());
+            ImageIcon originalIcon = new ImageIcon(imageBytes);
+            Image originalImage = originalIcon.getImage();
+
+            // Resize the image to fit inside imagePanel
+            int panelWidth = 167;
+            int panelHeight = 71;
+            Image resizedImage = originalImage.getScaledInstance(panelWidth, panelHeight, Image.SCALE_SMOOTH);
+
+            // Set the resized image to imagePanel
+            ImageIcon resizedIcon = new ImageIcon(resizedImage);
+            imagePanel.setIcon(resizedIcon);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -106,23 +115,22 @@ public class carDescription extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(activeStats, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43))
+                .addGap(17, 17, 17))
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(rent)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2))
-                            .addComponent(imagePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(45, 45, 45)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(rent, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton2))
+                    .addComponent(imagePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -135,9 +143,9 @@ public class carDescription extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(rent)
                     .addComponent(jButton2))
-                .addGap(30, 30, 30)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(activeStats, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18))
+                .addGap(42, 42, 42))
         );
     }// </editor-fold>//GEN-END:initComponents
 
